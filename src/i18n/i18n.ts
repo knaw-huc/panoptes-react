9ac179f4692a0d58f68ef6a80a38d43e07e67378
Johan List <johan.list@di.huc.knaw.nl>
2026-01-19 11:19:18 +0100

    Moved i18n files back to src; added src/i18n/i18n.ts to package.json sideEffects props to avoid having the bundler tree-shake this out (i18n.ts is imported only for its side effect, namely initializing i18n)

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,12 +1,15 @@
 import i18n from "i18next";
-import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(HttpBackend).use(LanguageDetector).use(initReactI18next).init({
+import en from "./locales/en/common.json";
+import nl from "./locales/nl/common.json";
+
+i18n.use(LanguageDetector).use(initReactI18next).init({
     fallbackLng: "en",
-    backend: {
-        loadPath: "/locales/{{lng}}/{{ns}}.json",
+    resources: {
+        en: { common: en },
+        nl: { common: nl },
     },
     defaultNS: "common",
     ns: ["common"],
